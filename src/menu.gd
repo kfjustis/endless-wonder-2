78e141249a1da5faf4cde46e78277e0cb784f914
kfjustis <kfjustis@gmail.com>
2017-09-03 17:55:58 -0500

    Added title to menu screen + improved text spacing

--- src/menu.gd
+++ src/menu.gd
@@ -7,9 +7,12 @@ extends Node2D
 
 onready var screen_res = Vector2(Globals.get("display/width"), Globals.get("display/height"))
 onready var o_bg = get_node("options_bg")
+onready var c_title = get_node("options_bg/title_container")
+onready var t_title = get_node("options_bg/title_container/l_title")
 
 # percent size of the main screen resolution
 var SCREEN_RES_PERC = 0.75
+
 var OBG_OPACITY = 0.75
 
 func _ready():
@@ -26,7 +29,9 @@ func setup_options():
 	# set size for the option bg frame
 	var obg_size = Vector2(screen_res.x * SCREEN_RES_PERC, screen_res.y * SCREEN_RES_PERC)
 	var obg_centered = Vector2((screen_res.x/2) - (obg_size.x/2), (screen_res.y/2) - (obg_size.y/2))
-	
 	o_bg.set_size(obg_size)
 	o_bg.set_pos(obg_centered)
-	o_bg.set_self_opacity(OBG_OPACITY)
+	
+	# set text options
+	var title_centered = Vector2((o_bg.get_size().x/2) - (c_title.get_size().x/2), (o_bg.get_size().y/9) - (c_title.get_size().y/2))
+	c_title.set_pos(title_centered)
