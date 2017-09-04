4d386bdc66214b87f6ebc212ad2c154eed8ad1a0
kfjustis <kfjustis@gmail.com>
2017-09-03 20:21:47 -0500

    Updated CL, 'New Game' main menu option works now

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,10 @@
+## v0.2.1 - Sep 3, 2017
+* Menu options become highlighted when moused over
+* "New Game" option loads the first scene
+
 ## v0.2.0 - Sep 3, 2017
 * Added background for main menu options
-* Added basic main menu options
+* Added basic main menu options labels
 
 ## v0.1.0 - Oct 26, 2016
 * Added intro cutscene
--- src/menu.gd
+++ src/menu.gd
@@ -5,24 +5,45 @@ extends Node2D
 # var a=2
 # var b="textvar"
 
-onready var screen_res = Vector2(Globals.get("display/width"), Globals.get("display/height"))
-onready var o_bg = get_node("options_bg")
-onready var c_title = get_node("options_bg/title_container")
-onready var t_title = get_node("options_bg/title_container/l_title")
-
+# constants
 # percent size of the main screen resolution
 var SCREEN_RES_PERC = 0.75
+var BUTTON_BG_PADDING = 0.80
+# project screen resolution
+onready var screen_res = Vector2(Globals.get("display/width"), Globals.get("display/height"))
+# translucent background for menu items
+onready var o_bg = get_node("options_bg")
+# container for title text
+onready var c_title = get_node("options_bg/title_container")
+# container for menu items
+onready var t_title = get_node("options_bg/title_container/l_title")
 
-var OBG_OPACITY = 0.75
+# menu items
+# labels
+onready var l_ng = get_node("options_bg/options_container/l_new_game")
+onready var l_lg = get_node("options_bg/options_container/l_load_game")
+onready var l_opts = get_node("options_bg/options_container/l_options")
+onready var l_creds = get_node("options_bg/options_container/l_credits")
+# buttons
+onready var b_ng = get_node("options_bg/options_container/l_new_game/b_new_game")
+onready var b_lg = get_node("options_bg/options_container/l_load_game/b_load_game")
+onready var b_opts = get_node("options_bg/options_container/l_options/b_options")
+onready var b_creds = get_node("options_bg/options_container/l_credits/b_credits")
 
 func _ready():
 	get_node("stream_player").play()
+	# setup the menu graphics and styling
 	setup_options()
 	set_process(true)
 
 func _process(delta):
+	# handle escape
 	if (Input.is_action_pressed("close_game")):
 		get_tree().quit()
+	
+	# handle menu items
+	#if (check_mouse_bounds(n_ng)):
+	#	print("MOUSE WAS OVER NEW GAME")
 
 # styles the background/options stuff for the menu screen
 func setup_options():
@@ -33,5 +54,28 @@ func setup_options():
 	o_bg.set_pos(obg_centered)
 	
 	# set text options
+	# title
 	var title_centered = Vector2((o_bg.get_size().x/2) - (c_title.get_size().x/2), (o_bg.get_size().y/9) - (c_title.get_size().y/2))
 	c_title.set_pos(title_centered)
+	# button labels
+	# new game
+	l_ng.set_size(l_ng.get_size() + l_ng.get_size() * BUTTON_BG_PADDING)
+	b_ng.set_pos(l_ng.get_pos())
+	b_ng.set_size(l_ng.get_size())
+	b_ng.connect("pressed", self, "load_new_game")
+	#l_ng.connect("pressed", self, "load_new_game")
+	# load game
+	l_lg.set_size(l_lg.get_size() + l_lg.get_size() * BUTTON_BG_PADDING)
+	b_lg.set_pos(l_lg.get_pos() * l_ng.get_pos())
+	b_lg.set_size(l_lg.get_size())
+	# options
+	l_opts.set_size(l_opts.get_size() + l_opts.get_size() * BUTTON_BG_PADDING)
+	b_opts.set_pos(b_opts.get_pos() * l_ng.get_pos())
+	b_opts.set_size(l_opts.get_size())
+	# credits
+	l_creds.set_size(l_creds.get_size() + l_creds.get_size() * BUTTON_BG_PADDING)
+	b_creds.set_pos(b_creds.get_pos() * l_ng.get_pos())
+	b_creds.set_size(l_creds.get_size())
+
+func load_new_game():
+	get_tree().change_scene("title_1_space.tscn")
