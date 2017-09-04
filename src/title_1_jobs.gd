6f7075b76367c19c4a415fb0be94d85081c0a571
kfjustis <kfjustis@gmail.com>
2017-09-04 03:23:33 -0500

    Bug making fist punch

--- src/title_1_jobs.gd
+++ src/title_1_jobs.gd
@@ -9,10 +9,44 @@ onready var screen_res = Vector2(Globals.get("display/width"), Globals.get("disp
 onready var slide_left = get_node("dark_left")
 onready var slide_right = get_node("dark_right")
 
+# setup hand
+onready var hand = get_node("hand")
+# tested for this value
+onready var hand_end_pos = Vector2(screen_res.x/2-hand.get_pos().x/4.0, screen_res.y-hand.get_pos().y/2 + 62.0)
+var hand_dir = Vector2(0,0)
+
+# scene controls
+var player_control = false
+
 func _ready():
+	# setup shades
 	var shade_size = Vector2(screen_res.x/2, screen_res.y)
 	var shade_left_pos = Vector2(0,0)
 	var shade_right_pos = Vector2(screen_res.x/2, 0)
 	slide_left.set_size(shade_size)
 	slide_right.set_size(shade_size)
 	slide_right.set_pos(shade_right_pos)
+	
+	set_process(true)
+	set_fixed_process(true)
+
+func _process(delta):
+	# handle escape
+	if (Input.is_action_pressed("close_game")):
+		get_tree().quit()
+	
+	if (Input.is_key_pressed(KEY_UP)):
+		print("-----> UP pressed")
+		hand_dir = Vector2(hand_end_pos.x, hand_end_pos.y - 150.0)
+	
+	hand_dir = Vector2(hand_end_pos.x, hand_end_pos.y)
+	
+func _fixed_process(delta):
+	# handle player
+	if (player_control):
+		print("Player has control")
+		hand.set_pos(hand_dir)
+
+
+func end_animation():
+	player_control = true
